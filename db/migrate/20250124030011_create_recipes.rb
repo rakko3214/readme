d56d566a0ff0d1f6f60d2ee17aa1b0aaa05c693e
河本行平 <me32140941@gmail.com>
2025-01-24 21:41:32 +0900

    lint修正

--- db/migrate/20250124030011_create_recipes.rb
+++ db/migrate/20250124030011_create_recipes.rb
@@ -2,8 +2,8 @@ class CreateRecipes < ActiveRecord::Migration[7.2]
   def change
     create_table :recipes do |t|
       t.string :title, null: false
-      t.text :content, null:false
-      t.integer :cooking_time, null:false
+      t.text :content, null: false
+      t.integer :cooking_time, null: false
 
       t.timestamps
     end
