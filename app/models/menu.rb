ea59584133d03a29d2994d3f5efed13345c2de49
河本行平 <me32140941@gmail.com>
2025-02-06 19:58:46 +0900

    error_messageno 作成

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,5 @@
 class Menu < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 255 }
   belongs_to :user
   has_and_belongs_to_many :recipes
 end
--- db/migrate/20250202045356_create_menus.rb
+++ db/migrate/20250202045356_create_menus.rb
@@ -1,7 +1,7 @@
 class CreateMenus < ActiveRecord::Migration[7.2]
   def change
     create_table :menus do |t|
-      t.string :title
+      t.string :title, null: false
       t.references :user, foreign_key: true
       t.references :recipe, foreign_key: true
 
