bc23cf8d50c0d52fc4b5174297d2920613ea9900
河本行平 <me32140941@gmail.com>
2025-02-22 12:06:16 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Myapp
     config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
+    onfig.middleware.delete ActionDispatch::HostAuthorization
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
