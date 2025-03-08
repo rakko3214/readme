1366a2e35509f8c856d9dd46128b0f5f661f6539
河本行平 <me32140941@gmail.com>
2025-03-08 12:30:56 +0900

    lint修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # /users/sign_up
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
   end
 
   def not_authenticated
