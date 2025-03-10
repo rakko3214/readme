afe5ad3a8467e21fd4f966ecb83f6bbc83d79614
河本行平 <me32140941@gmail.com>
2025-03-10 12:10:08 +0900

    google認証機能の作成

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # callback for google
+  def google_oauth2
+    callback_for(:google)
+  end
+
+  def callback_for(provider)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user, event: :authentication
+    set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
