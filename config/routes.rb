7d6f113d564576e9278e8bc50947e9bcc072c7ad
河本行平 <me32140941@gmail.com>
2025-03-11 12:42:10 +0900

    newファイルの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks"
+    omniauth_callbacks: "users/omniauth_callbacks",
+    passwords: "devise/passwords"
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
