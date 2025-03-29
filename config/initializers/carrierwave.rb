bf04494f3187fbf1f4a2478839fb014244a3bdbb
河本行平 <me32140941@gmail.com>
2025-03-29 14:34:27 +0900

    carrywave設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     provider:              "AWS",
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region:                ENV["AWS_REGION"],
+    region:                ENV["AWS_REGION"]
   }
   config.fog_directory  = ENV["AWS_BUCKET"]
 end
