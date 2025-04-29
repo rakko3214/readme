cf082117de8fcb816ac18c669a21d4e85dc28ece
河本行平 <me32140941@gmail.com>
2025-04-29 17:42:30 +0900

    献立情報を自分ののみ表示に変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,3 +12,21 @@ window.addEventListener('turbo:render', ()=>{
   const spinner = document.getElementById("loading");
   spinner.classList.add("loaded");
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  // チェックボックスの状態を復元
+  console.log("d");
+  const recipeCheckboxes = document.querySelectorAll('input[type=checkbox][name="menu[recipe_ids][]"]');
+  recipeCheckboxes.forEach(checkbox => {
+    const recipeId = checkbox.value;
+    const isChecked = localStorage.getItem(`recipe_${recipeId}`) === 'true';
+    checkbox.checked = isChecked;
+  });
+
+  // チェックボックスの変更を検知して状態を保存
+  recipeCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function() {
+      localStorage.setItem(`recipe_${this.value}`, this.checked);
+    });
+  });
+});
